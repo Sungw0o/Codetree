2bde0e0deaba7f3dc4a5ee05b0d0d7baef577e72
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-01-26 00:54:37 +0900

    Title: 문자열 출력하기, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260126/문자열 출력하기/print-string.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            sb.append("LeebrosCode").append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
