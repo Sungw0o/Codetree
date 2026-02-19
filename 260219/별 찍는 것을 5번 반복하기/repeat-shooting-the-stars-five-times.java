dbf809ba3d17a1b7a60de612035d207eb68d6ff7
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-19 15:52:23 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260219/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOExeption {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+
+        for(int i = 0 ; i < 5; i++){
+            print(n);
+        }
+
+        br.close();
+    }
+
+    public static void print(int n){
+
+        System.out.println("**********");
+    }
+}
