320714926dabc5a7ed0993228e4fcf03c6cb7da4
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-19 15:55:26 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 81ms, Memory: 9MB, Status: Passed - Codetree

--- 260219/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
+++ 260219/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        //int N = Integer.parseInt(br.readLine());
 
 
         for(int i = 0 ; i < 5; i++){
