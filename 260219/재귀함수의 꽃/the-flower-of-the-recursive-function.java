6a911a775b494b75aa72d61a8e940c850c823023
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-19 14:02:26 +0900

    Title: 재귀함수의 꽃, Time: 88ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260219/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	private static BufferedReader br;
+	private static BufferedWriter bw; 
+	private static StringTokenizer st;
+	private static StringBuilder sb;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		
+		print(N);
+		System.out.println(sb);
+	}
+	
+	public static void print(int n) {
+		
+		if(n == 0) return;
+		sb.append(n);
+        sb.append(" ");
+		print(n-1);
+        sb.append(n);
+        sb.append(" ");
+	}
+}
