ff0c1389acb0a5901d3ba3531547fad96c9c642c
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-19 15:21:18 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260219/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	private static BufferedReader br;
+	private static BufferedWriter bw; 
+	private static StringTokenizer st;
+	private static StringBuilder sb;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		//sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		
+		System.out.println(print(N));
+	}
+	
+	public static int print(int n) {
+		
+		if(n==1 || n==2) return 1;
+		
+		return print(n-1) + print(n-2);
+	}
+}
