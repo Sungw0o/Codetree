268e6ca8da0131a03cfca71a9851de8d972304cc
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-19 15:33:27 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260219/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	private static BufferedReader br;
+	private static BufferedWriter bw; 
+	private static StringTokenizer st;
+	private static StringBuilder sb;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		//sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		
+		System.out.println(print(N));
+	}
+	
+	public static int print(int n) {
+		
+		if (n <= 2) {
+            return n;
+        }
+		
+		return n + print(n-2);
+	}
+}
