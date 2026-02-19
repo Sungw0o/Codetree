affdaabf0676ef06c1944c08100538e8aaf9bcc6
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-19 14:12:28 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260219/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	private static BufferedReader br;
+	private static BufferedWriter bw; 
+	private static StringTokenizer st;
+	private static StringBuilder sb;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine()); 
+		br.close();
+		
+		print(N);
+		System.out.println(sb);
+	}
+	
+	public static void print(int n) {
+		
+		if(n == 0) return; // 종료 조건
+		
+		for(int i = 0; i < n; i++) {
+			sb.append("*").append(" ");
+		}
+		sb.append("\n");
+		
+		print(n-1);
+		
+		for(int i = 0; i < n; i++) {
+			sb.append("*").append(" ");
+		}
+		sb.append("\n");
+	}
+}
