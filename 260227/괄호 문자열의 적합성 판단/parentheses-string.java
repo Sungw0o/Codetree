66862b61b3a5cccfca55404220295382865ad531
Sungw0o <131230124+Sungw0o@users.noreply.github.com>
2026-02-27 16:48:10 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 94ms, Memory: 9MB, Status: Runtime Error - Codetree

---
+++ 260227/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Main {
+	
+	private static int N;
+	private static ArrayList<Integer> arr = new ArrayList<>();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		char[] arr = br.readLine().toCharArray();
+		
+		Stack<Character> stack =  new Stack<>();
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] == '(') {
+				stack.push(arr[i]);
+			}
+			else {
+				stack.pop();
+			}
+		}
+		
+		String ans = stack.isEmpty() ? "Yes" : "No";
+		
+		System.out.println(ans);
+		
+		
+		//System.out.println(sb);
+		br.close();
+	}
+}
